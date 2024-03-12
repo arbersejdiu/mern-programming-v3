fc2f763bc7cefa642a3bc139b6c62e8b1f9c969d
Arber Sejdiu <arbersejdiu.upwork@gmail.com>
2024-03-12 21:47:53 +0100

    Show todos

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Todo = require("../models/Todo");
 
+// Show todos
 router.get("/", async (req, res) => {
   const allTodos = await Todo.find();
   res.render("index.ejs", { Todo: allTodos });
